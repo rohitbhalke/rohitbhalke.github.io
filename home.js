fcb0ef8719d28db1db8c6bf5cf2223236a458ead
Rohit Bhalke <rohitbhalke@gmail.com>
2017-09-08 15:23:16 +0530

    Initial commit

---
+++ home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '/Users/rbo2913/Documents/My Directory/React/MyWebsite/scripts/App.js';
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
