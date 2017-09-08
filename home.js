f4e31ba611a37cc2382fb09147eea8fe1f965381
Rohit Bhalke <rohitbhalke@gmail.com>
2017-09-08 18:08:26 +0530

    Delete Everything

--- home.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '/Users/rbo2913/Documents/My Directory/React/MyWebsite/scripts/App.js';
-
-
-ReactDOM.render(<App />, document.getElementById('app'));
